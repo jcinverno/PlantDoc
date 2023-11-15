024949fa07fc95605e4d54f36b7dea9fe5bc131e
jcinverno <jc.inverno@campus.fct.unl.pt>
2023-11-15 19:21:42 +0000

    makes wrong predictions but it predicts something... hurray :)

--- app/src/main/java/com/example/bottomnavigationdemo/CameraFragment.java
+++ app/src/main/java/com/example/bottomnavigationdemo/CameraFragment.java
@@ -2,17 +2,14 @@ package com.example.bottomnavigationdemo;
 
 import android.Manifest;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -28,14 +25,10 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-import android.provider.MediaStore;
 
 import com.example.bottomnavigationdemo.ml.TfliteModel;
 
@@ -51,6 +44,27 @@ public class CameraFragment extends Fragment {
     ImageView mImageView;
     Button mCaptureBtn;
     Button mPredictBtn;
+
+    String[] classes = {"bacspot",
+            "black_measles",
+            "black_spot",
+            "cedar_rust",
+            "commonRust",
+            "early_blight",
+            "gray_leaf_spot",
+            "healthy",
+            "isariopsis_leaf_spot",
+            "late_blight",
+            "leaf_mold",
+            "leaf_scorch",
+            "mosaic_virus",
+            "northern_leaf_blight",
+            "powdery_mildew",
+            "scab",
+            "septoria_leaf_spot",
+            "target_spot",
+            "two_spotted_spider_mite",
+            "yellow_leaf_curl_virus"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -84,7 +98,45 @@ public class CameraFragment extends Fragment {
         mPredictBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(requireContext(), "Predict", Toast.LENGTH_SHORT).show();
+                try {
+                    TfliteModel model = TfliteModel.newInstance(requireContext());
+
+                    // Creates inputs for reference.
+                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 64, 64, 3}, DataType.FLOAT32);
+                    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+                    byteBuffer.order(ByteOrder.nativeOrder());
+
+                    int[] intValues = new int[bitmap.getWidth()*bitmap.getHeight()];
+                    bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+                    int pixel = 0;
+
+                    for (int i = 0; i < imageSize; i++) {
+                        for (int j = 0; j < imageSize; j++) {
+
+                            if (pixel < intValues.length) {
+                                int val = intValues[pixel++];
+                                byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                                byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                                byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                            }
+                        }
+                    }
+
+                    inputFeature0.loadBuffer(byteBuffer);
+
+                    // Runs model inference and gets the result.
+                    TfliteModel.Outputs outputs = model.process(inputFeature0);
+                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+                    // Process the output (you may need to adjust this based on your model's output)
+                    prediction(outputFeature0.getFloatArray());
+
+                    // Release model resources.
+                    model.close();
+
+                } catch (Exception e) {
+                    Toast.makeText(requireContext(), "" + e, Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -128,4 +180,17 @@ public class CameraFragment extends Fragment {
         }
     }
 
+    private void prediction(float[] probabilities) {
+
+        int maxPos = -1;
+        float maxProb = 0;
+
+        for (int i = 0; i < probabilities.length; i++) {
+            if (probabilities[i] > maxProb) {
+                maxProb = probabilities[i];
+                maxPos = i;
+            }
+        }
+        Toast.makeText(requireContext(), "Prediction: " + classes[maxPos], Toast.LENGTH_SHORT).show();
+    }
 }
