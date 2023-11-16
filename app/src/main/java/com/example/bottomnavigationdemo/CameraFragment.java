5a03b718a59a5cf1aac7994732b51040966e32a6
jcinverno <jc.inverno@campus.fct.unl.pt>
2023-11-16 09:24:11 +0000

    goes to the gallery to fetch images

--- app/src/main/java/com/example/bottomnavigationdemo/CameraFragment.java
+++ app/src/main/java/com/example/bottomnavigationdemo/CameraFragment.java
@@ -13,6 +13,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.view.View;
 import android.widget.Toast;
@@ -36,35 +40,36 @@ import com.example.bottomnavigationdemo.ml.TfliteModel;
 
 public class CameraFragment extends Fragment {
 
-    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_PERMISSION_CODE = 1000;
     private static final int IMAGE_CAPTURE_CODE = 1001;
+    private static final int GALLERY_PERMISSION_CODE = 2;
     private Uri image_uri;
     private Bitmap bitmap;
     int imageSize = 64;
     ImageView mImageView;
     Button mCaptureBtn;
-    Button mPredictBtn;
+    Button mGalleryBtn;
 
-    String[] classes = {"bacspot",
-            "black_measles",
-            "black_spot",
-            "cedar_rust",
-            "commonRust",
-            "early_blight",
-            "gray_leaf_spot",
-            "healthy",
-            "isariopsis_leaf_spot",
-            "late_blight",
-            "leaf_mold",
-            "leaf_scorch",
-            "mosaic_virus",
-            "northern_leaf_blight",
-            "powdery_mildew",
-            "scab",
-            "septoria_leaf_spot",
-            "target_spot",
-            "two_spotted_spider_mite",
-            "yellow_leaf_curl_virus"};
+    String[] classes = {"Back Spot",
+            "Black_measles",
+            "Black Spot",
+            "Cedar Rust",
+            "Common Rust",
+            "Early Blight",
+            "Gray Leaf Spot",
+            "Healthy",
+            "Isariopsis Leaf Spot",
+            "Late Blight",
+            "Leaf Mold",
+            "Leaf Scorch",
+            "Mosaic Virus",
+            "Northern Leaf Blight",
+            "Powdery Mildew",
+            "Scab",
+            "Septoria Leaf Spot",
+            "Target Spot",
+            "Two Spotted Spider Mite",
+            "Yellow Leaf Curl Virus"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -74,7 +79,7 @@ public class CameraFragment extends Fragment {
 
         mImageView = view.findViewById(R.id.image_view);
         mCaptureBtn = view.findViewById(R.id.capture_image_btn);
-        mPredictBtn = view.findViewById(R.id.predict_image_btn);
+        mGalleryBtn = view.findViewById(R.id.gallery_image_btn);
 
         mCaptureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +90,7 @@ public class CameraFragment extends Fragment {
                             ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
                                     PackageManager.PERMISSION_DENIED) {
                         String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                        requestPermissions(permission, PERMISSION_CODE);
+                        requestPermissions(permission, IMAGE_PERMISSION_CODE);
                     } else {
                         openCamera();
                     }
@@ -95,47 +100,23 @@ public class CameraFragment extends Fragment {
             }
         });
 
-        mPredictBtn.setOnClickListener(new View.OnClickListener() {
+        mGalleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    TfliteModel model = TfliteModel.newInstance(requireContext());
-
-                    // Creates inputs for reference.
-                    TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 64, 64, 3}, DataType.FLOAT32);
-                    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
-                    byteBuffer.order(ByteOrder.nativeOrder());
-
-                    int[] intValues = new int[bitmap.getWidth()*bitmap.getHeight()];
-                    bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-                    int pixel = 0;
-
-                    for (int i = 0; i < imageSize; i++) {
-                        for (int j = 0; j < imageSize; j++) {
-
-                            if (pixel < intValues.length) {
-                                int val = intValues[pixel++];
-                                byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
-                                byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
-                                byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
-                            }
-                        }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED ||
+                            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED) {
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permission, GALLERY_PERMISSION_CODE);
+                    } else {
+                        openGallery();
                     }
-
-                    inputFeature0.loadBuffer(byteBuffer);
-
-                    // Runs model inference and gets the result.
-                    TfliteModel.Outputs outputs = model.process(inputFeature0);
-                    TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-
-                    // Process the output (you may need to adjust this based on your model's output)
-                    prediction(outputFeature0.getFloatArray());
-
-                    // Release model resources.
-                    model.close();
-
-                } catch (Exception e) {
-                    Toast.makeText(requireContext(), "" + e, Toast.LENGTH_SHORT).show();
+                } else {
+                    openGallery();
                 }
             }
         });
@@ -154,29 +135,111 @@ public class CameraFragment extends Fragment {
         startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
     }
 
+    private void openGallery() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Gallery");
+
+        image_uri = requireContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), GALLERY_PERMISSION_CODE);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openCamera();
-            } else {
-                Toast.makeText(requireContext(), "Permission Denied...", Toast.LENGTH_SHORT).show();
-            }
+
+        switch (requestCode) {
+            case IMAGE_CAPTURE_CODE:
+                handleCameraPermissionResult(grantResults);
+                break;
+            case GALLERY_PERMISSION_CODE:
+                handleGalleryPermissionResult(grantResults);
+                break;
         }
     }
+
+    private void handleCameraPermissionResult(int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+        } else {
+            Toast.makeText(requireContext(), "Camera Permission Denied...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void handleGalleryPermissionResult(int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openGallery();
+        } else {
+            Toast.makeText(requireContext(), "Gallery Permission Denied...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == IMAGE_CAPTURE_CODE && resultCode == AppCompatActivity.RESULT_OK) {
             try {
-                // Set the captured image to the ImageView
                 bitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), image_uri);
                 mImageView.setImageBitmap(bitmap);
+                makePrediction();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        else if (requestCode == GALLERY_PERMISSION_CODE && resultCode == AppCompatActivity.RESULT_OK) {
+            try {
+                image_uri = data.getData();
+                bitmap = MediaStore.Images.Media.getBitmap(requireContext().getContentResolver(), image_uri);
+                mImageView.setImageBitmap(bitmap);
+                makePrediction();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void makePrediction(){
+        try {
+            TfliteModel model = TfliteModel.newInstance(requireContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 64, 64, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[bitmap.getWidth()*bitmap.getHeight()];
+            bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+            int pixel = 0;
+
+            for (int i = 0; i < imageSize; i++) {
+                for (int j = 0; j < imageSize; j++) {
+
+                    if (pixel < intValues.length) {
+                        int val = intValues[pixel++];
+                        byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                        byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                        byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                    }
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            TfliteModel.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            prediction(outputFeature0.getFloatArray());
+
+            model.close();
+
+        } catch (Exception e) {
+            Toast.makeText(requireContext(), "" + e, Toast.LENGTH_SHORT).show();
         }
     }
 
